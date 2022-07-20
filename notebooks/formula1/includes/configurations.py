206e22c5f90247e70300e5ad97e5e2b4f4a0b22b
Lucky Khanyile <luckykhanyile@gmail.com>
2022-07-20 03:53:27 +0200

    Update: 0.create_database.sql notebooks

---
+++ notebooks/formula1/includes/configurations.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path="/mnt/4mula1dl/raw"
+processed_folder_path ="/mnt/4mula1dl/processed"
+presentation_folder_path ="/mnt/4mula1dl/presentation"
+
+# COMMAND ----------
+
