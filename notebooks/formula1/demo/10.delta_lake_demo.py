8ca8b021c8dc00820d16222a6fdcc2aa5eda3b16
Lucky Khanyile <luckykhanyile@gmail.com>
2022-07-24 05:28:42 +0200

    Update: 0.create_database.sql notebooks

---
+++ notebooks/formula1/demo/10.delta_lake_demo.py
@@ -0,0 +1,67 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC drop database f1_demo2
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE DATABASE IF NOT EXISTS f1_demo
+# MAGIC LOCATION '/mnt/4mula1dl/demo';
+
+# COMMAND ----------
+
+results_df = spark.read.option("inferSchema", True).json("/mnt/4mula1dl/raw/2021-03-28/results.json")
+
+# COMMAND ----------
+
+results_df.write.format("delta").mode("overwrite").saveAsTable("f1_demo.results_managed")
+
+# COMMAND ----------
+
+results_df.write.format("delta").mode("overwrite").partitionBy("constructorId").saveAsTable("f1_demo.results_managed_partitioned")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW PARTITIONS f1_demo.results_managed_partitioned
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from f1_demo.results_managed;
+
+# COMMAND ----------
+
+results_df.write.format("delta").mode("overwrite").save("/mnt/4mula1dl/demo/results_external")
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/4mula1dl/demo
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE f1_demo.results_external
+# MAGIC USING DELTA
+# MAGIC LOCATION "/mnt/4mula1dl/demo/results_external" 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from f1_demo.results_external
+
+# COMMAND ----------
+
+results_external_demo = spark.read.format("delta").load("/mnt/4mula1dl/demo/results_external")
+
+# COMMAND ----------
+
+display(results_external_demo)
+
+# COMMAND ----------
+
