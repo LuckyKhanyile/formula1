62a6d2f967823aa9b40699bb01d160cf7793647c
Lucky Khanyile <luckykhanyile@gmail.com>
2022-08-01 05:51:44 +0200

    Update: 0.create_database.sql notebooks

--- notebooks/formula1/demo/10.delta_lake_demo.py
+++ notebooks/formula1/demo/10.delta_lake_demo.py
@@ -94,3 +94,150 @@ deltaTable.update("position<=10",{"points" : "20-position"})
 
 # COMMAND ----------
 
+deltaTable.delete("position=0")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import upper
+
+drivers_day1_df = spark.read.option("inferSchema","True").json("/mnt/4mula1dl/raw/2021-03-28/drivers.json").filter("driverId<=10")\
+                            .select("driverId","dob","name.forename","name.surname") 
+
+# COMMAND ----------
+
+display(drivers_day1_df)
+
+# COMMAND ----------
+
+drivers_day1_df.createOrReplaceTempView("drivers_day1")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import upper
+
+drivers_day2_df = spark.read.option("inferSchema","True").json("/mnt/4mula1dl/raw/2021-03-28/drivers.json").filter("driverId BETWEEN 6 AND 15")\
+                            .select("driverId","dob",upper("name.forename").alias("forename"),upper("name.surname").alias("surname")) 
+
+# COMMAND ----------
+
+drivers_day2_df.createOrReplaceTempView("drivers_day2")
+
+# COMMAND ----------
+
+display(drivers_day2_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import upper
+
+drivers_day3_df = spark.read.option("inferSchema","True").json("/mnt/4mula1dl/raw/2021-03-28/drivers.json")\
+                            .filter("driverId BETWEEN 1 AND 5 OR driverId BETWEEN 16 AND 20")\
+                            .select("driverId","dob",upper("name.forename").alias("forename"),upper("name.surname").alias("surname")) 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE IF NOT EXISTS f1_demo.drivers_merge(
+# MAGIC driverId INT,
+# MAGIC dob DATE,
+# MAGIC forename STRING,
+# MAGIC surname STRING,
+# MAGIC createdDate DATE,
+# MAGIC updatedDate Date
+# MAGIC )
+# MAGIC USING DELTA
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC MERGE INTO f1_demo.drivers_merge tgt
+# MAGIC USING drivers_day1 upd
+# MAGIC ON upd.driverId = tgt.driverId
+# MAGIC WHEN MATCHED THEN
+# MAGIC   UPDATE SET tgt.dob = upd.dob,
+# MAGIC              tgt.forename = upd.forename,
+# MAGIC              tgt.surname = upd.surname,
+# MAGIC              tgt.updatedDate = current_timestamp
+# MAGIC WHEN NOT MATCHED
+# MAGIC THEN INSERT (driverId,dob,forename,surname,createdDate) VALUES (driverId,dob,forename,surname,current_timestamp)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from f1_demo.drivers_merge
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC MERGE INTO f1_demo.drivers_merge tgt
+# MAGIC USING drivers_day2 upd
+# MAGIC ON upd.driverId = tgt.driverId
+# MAGIC WHEN MATCHED THEN
+# MAGIC   UPDATE SET tgt.dob = upd.dob,
+# MAGIC              tgt.forename = upd.forename,
+# MAGIC              tgt.surname = upd.surname,
+# MAGIC              tgt.updatedDate = current_timestamp
+# MAGIC WHEN NOT MATCHED
+# MAGIC THEN INSERT (driverId,dob,forename,surname,createdDate) VALUES (driverId,dob,forename,surname,current_timestamp)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from f1_demo.drivers_merge
+
+# COMMAND ----------
+
+from delta.tables import DeltaTable
+
+deltaTable = DeltaTable.forPath(spark, '/mnt/4mula1dl/demo/drivers_merge')
+
+
+deltaTable.alias('tgt') \
+  .merge(
+    drivers_day3_df.alias('upd'),
+    'tgt.driverId = upd.driverId'
+  ) \
+  .whenMatchedUpdate(set =
+    {
+             "dob" : "upd.dob",
+             "forename" : "upd.forename",
+             "surname" : "upd.surname",
+             "updatedDate" : "current_timestamp()"
+    }
+  ) \
+  .whenNotMatchedInsert(values =
+    {
+         "driverId":"upd.driverId",
+         "dob" : "upd.dob",
+         "forename" : "upd.forename",
+         "surname" : "upd.surname",
+         "createdDate" : "current_timestamp()"
+    }
+  ) \
+  .execute()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from f1_demo.drivers_merge
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC desc history f1_demo.drivers_merge;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from f1_demo.drivers_merge  Version as of "3"
+
+# COMMAND ----------
+
+df = spark.read.format("delta").option("version","1").load("/mnt/4mula1dl/demo/drivers_merge")
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
